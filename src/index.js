2a53f5b0b92eab6f56edfcf0e83b55364f93f164
KhangDoVan <dvk10102000@gmail.com>
2021-07-10 22:39:49 +0700

    forth update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const app = express()
 const port = 3000;
 
 
-
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 app.engine('hbs', handlebars({
@@ -17,15 +20,24 @@ app.set('views',path.join(__dirname,'resources/views'));
 
 
  
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   
   res.render('home');
 })
+
 app.get('/contact', (req, res) => {
   
   res.render('contact');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/search', (req, res) => {
+  
+  res.render('search');
 })
+
+app.post('/home', (req, res) => {
+  
+  res.render('contact');
+})
+
+app.listen(port);
